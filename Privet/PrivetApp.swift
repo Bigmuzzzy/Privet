75e644898628d3a324795169a8ebe8772f4c3306
Mike <pustozvonov@gmail.com>
2025-12-06 21:37:49 +0300

    Initial Commit

---
+++ Privet/PrivetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivetApp.swift
+//  Privet
+//
+//  Created by Mike on 06.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct PrivetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
